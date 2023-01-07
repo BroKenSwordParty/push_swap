3181165c0615ded514036e9e485eb0ea900bb59b
jchamorr <77245848+BroKenSwordParty@users.noreply.github.com>
2023-01-07 13:24:53 +0100

    fix order_fts

--- push_swap/order_fts.c
+++ push_swap/order_fts.c
@@ -11,32 +11,41 @@
 /* ************************************************************************** */
 
 #include "push_swap.h"
+#include "libft/libft.h"
 
 void	lets_order_this_numbers(t_push **s)
 {
-	t_stack	*stk;
+	int		n[3];
 
+	n[0] = (*s)->a->nb;
+	n[1] = (*s)->a->nxt->nb;
+	n[2] = (*s)->a->nxt->nxt->nb;
 	printf("PASAMOS POR AQUI\n");
-	stk = (*s)->a;
-	if (stk->nb > stk->nxt->nb && stk->nb > stk->nxt->nxt->nb)
+	if (n[0] > n[1] && n[1] < n[2] && n[0] < n[2])
 		rotate_a(&(*s)->a);
-	else if (stk->nb > stk->nxt->nb && stk->nxt->nb < stk->nxt->nxt->nb)
+	else if (n[0] > n[1] && n[1] > n[2])
 		swap_a(&(*s)->a);
-	else if (stk->nb > stk->nxt->nb && stk->nxt->nb > stk->nxt->nxt->nb)
+	else if (n[0] > n[1] && n[1] < n[2] && n[0] > n[2])
 	{
 		swap_a(&(*s)->a);
 		reverse_rotate_a(&(*s)->a);
 	}
-	else if (stk->nb < stk->nxt->nb && stk->nxt->nb > stk->nxt->nxt->nb)
+	else if (n[0] < n[1] && n[1] > n[2] && n[0] < n[2])
 	{
 		swap_a(&(*s)->a);
 		rotate_a(&(*s)->a);
 	}
-	else if (stk->nb < stk->nxt->nb && stk->nxt->nb > stk->nxt->nxt->nb)
+	else if (n[0] < n[1] && n[1] > n[2] && n[0] > n[2])
 		reverse_rotate_a(&(*s)->a);
 	// TENGO QUE REVISAR LOS CASOS ESPECÍFICOS PARA SER MÁS EXACTO PORQUE PUEDEN LLEGAR A FALLAR
 	// Y ENTRAR EN funciones que no son.
 }
+
+void	find_limits(t_push *s)
+{
+	
+}
+
 
 
 void	hardcoded_ordering(t_push *s)
@@ -56,6 +65,10 @@ void	hardcoded_ordering(t_push *s)
 		swap_a(&s->a);
 	if (s->index == 3)
 		lets_order_this_numbers(&s);
+	if (s->index == 5)
+		find_limits(&s);
+	// Voy a hacer 2 ft que busquen el numero más grande y el más pequeño (o una).
+	// Para hacer que ordene los de 5, pero teóricamente, ya que no hace make.
 	prueba = s->a;
 	printf("--------------------------------\n");
 	printf("LA LISTA DESPUÉS DE SER ORDENADA\n");
